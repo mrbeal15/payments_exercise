079ac9b886c9808ab7f7210aee0aea85138df788
Matt Beal <mrbeal15@gmail.com>
2017-10-22 09:57:19 -0500

    update loans controller

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,14 +1,25 @@
 class LoansController < ApplicationController
+  include LoansHelper
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: 'not_found', status: :not_found
   end
 
   def index
-    render json: Loan.all
+    @response = return_all_loans
+    render json: @response
   end
 
   def show
-    render json: Loan.find(params[:id])
+    loan = Loan.find(params[:id])
+    payments = return_all_payments(loan)
+    funded_amount = calculate_loan_amount(loan)
+
+    @response = {
+      outstanding_balance: funded_amount,
+      payments: payments
+    }
+    
+    render json: @response
   end
 end
