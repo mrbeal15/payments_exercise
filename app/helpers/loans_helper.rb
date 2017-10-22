105a4afe55c516b5c5b7d141d8bceb0cc1ced829
Matt Beal <mrbeal15@gmail.com>
2017-10-22 10:53:32 -0500

    refactor loan related methods

--- app/helpers/loans_helper.rb
+++ app/helpers/loans_helper.rb
@@ -2,23 +2,11 @@ module LoansHelper
 
   def return_all_loans
     Loan.all.map do |loan|
-      funded_amount = calculate_loan_amount(loan)
+      funded_amount = Loan.calculate_balance(loan)
       {
         id: loan.id,
         funded_amount: funded_amount
       }
-    end
-  end
-
-  def return_all_payments(loan)
-    loan.payments.map {|payment| payment}
-  end
-
-  def calculate_loan_amount(loan)
-    if !loan[:funded_amount].instance_of? BigDecimal
-      'Funded Amount must be a Decimal'
-    else
-      loan[:"funded_amount"].to_f
     end
   end
 end
